28af7bc398b4ceac1b26908c8132f8bd5d584dbf
Mohamed Mostafa <mohamed.mostafa.maher.999@gmail.com>
2022-07-24 19:55:14 +0200

    Common includes files -IntCtrl SysCtrl GPIO -

---
+++ tasks/Src/Common/MemoryMap.h
@@ -0,0 +1,196 @@
+/**********************************************************************************************************************
+
+ *  FILE DESCRIPTION
+ *  -------------------------------------------------------------------------------------------------------------------
+ *         File:  MemoryMap.h
+ *       Module:  MemoryMap
+ *
+ *  Description:  header file for TM4C123GH6PM Registers definition    
+ *  
+ *********************************************************************************************************************/
+#ifndef MEMORYMAP_H
+#define MEMORYMAP_H
+
+/**********************************************************************************************************************
+ * INCLUDES
+ *********************************************************************************************************************/
+#include "PlatformTypes.h"
+
+/**********************************************************************************************************************
+ *  GLOBAL DATA TYPES AND STRUCTURES
+ *********************************************************************************************************************/
+
+//type of each register
+#define RW__    volatile
+#define RO__    volatile
+#define WO__    volatile
+#define RWC__   volatile
+#define RW1C__  volatile
+#define W1C__  volatile
+
+//___________________________________
+//NVIC registers
+typedef struct 
+{
+    RW__ uint32 EN[5];
+    vuint32 reserved1[27];
+    RW__ uint32 DIS[5];
+    vuint32 reserved2[27];
+    RW__ uint32 PEND[5];
+    vuint32 reserved3[27];
+    RW__ uint32 UNPEND[5];
+    vuint32 reserved4[27];
+    RO__ uint32 ACTIV[5];
+    vuint32 reserved5[59];
+    RW__ uint32 PRI[35];
+    vuint32 reserved6[669];
+    WO__ uint32 SWTRIG;
+}NVIC_Type;
+
+//___________________________________
+//SCB registers
+typedef struct 
+{
+    RO__ uint32 CPUID;
+    RW__ uint32 INTCTRL;
+    RW__ uint32 VTABLE;
+    RW__ uint32 APINT;
+    RW__ uint32 SYSCTRL;
+    RW__ uint32 CFGCTRL;
+    RW__ uint32 SYSPRI1;
+    RW__ uint32 SYSPRI2;
+    RW__ uint32 SYSPRI3;
+    RW__ uint32 SYSHNDCTRL;
+    RW1C__ uint32 FAULTSTAT;
+    RW1C__ uint32 HFAULTSTAT;
+    vuint32 reserved2;
+    RW__ uint32 MMADDR;
+    RW__ uint32 FAULTADDR;
+}SCB_Type;
+//___________________________________
+//GPIO registers
+typedef struct{
+    RW__ uint32 GPIODATA[256];
+    RW__ uint32 GPIODIR;
+    RW__ uint32 GPIOIS;
+    RW__ uint32 GPIOIBE;
+    RW__ uint32 GPIOIEV;
+    RW__ uint32 GPIOIM;
+    RO__ uint32 GPIORIS;
+    RO__ uint32 GPIOMIS;
+    W1C__ uint32 GPIOICR;
+    RW__ uint32 GPIOAFSEL;
+    vuint32 reserved2[55];
+    RW__ uint32 GPIODR2R;
+    RW__ uint32 GPIODR4R;
+    RW__ uint32 GPIODR8R;
+    RW__ uint32 GPIOODR;
+    RW__ uint32 GPIOPUR;
+    RW__ uint32 GPIOPDR;
+    RW__ uint32 GPIOSLR;
+    RW__ uint32 GPIOEN;
+    RW__ uint32 GPIOLOCK;
+    vuint32 GPIOCR;
+    RW__ uint32 GPIOAMSEL;
+    RW__ uint32 GPIOPCTL;
+    RW__ uint32 GPIOADCCTL;
+    RW__ uint32 GPIODMACTL;
+    vuint32 reserved3[678];
+    RO__ uint32 GPIOPeriphID4;
+    RO__ uint32 GPIOPeriphID5;
+    RO__ uint32 GPIOPeriphID6;
+    RO__ uint32 GPIOPeriphID7;
+    RO__ uint32 GPIOPeriphID0;
+    RO__ uint32 GPIOPeriphID1;
+    RO__ uint32 GPIOPeriphID2;
+    RO__ uint32 GPIOPeriphID3;
+    RO__ uint32 GPIOPCellID0;
+    RO__ uint32 GPIOPCellID1;
+    RO__ uint32 GPIOPCellID2;
+    RO__ uint32 GPIOPCellID3;
+}GPIO_Type;
+
+//ONLY USED REGISTERES IS IMPLEMENTED
+typedef struct
+{
+    vuint32 reserved[383];
+    RW__ uint32 RCGCWD;
+    RW__ uint32 RCGCTIMER;
+    RW__ uint32 RCGCGPIO;
+    RW__ uint32 RCGCDMA;
+    vuint32 reserved1;
+    RW__ uint32 RCGCHIB;
+    RW__ uint32 RCGCUART;
+    RW__ uint32 RCGCSSI;
+    RW__ uint32 RCGCI2C;
+    vuint32 reserved2;
+    RW__ uint32 RCGCUSB;
+    vuint32 reserved3[2];
+    RW__ uint32 RCGCCAN;
+    RW__ uint32 RCGCADC;
+    RW__ uint32 RCGCACMP;
+    RW__ uint32 RCGCPWM;
+    RW__ uint32 RCGCQEI;
+    vuint32 reserved4[4];
+    RW__ uint32 RCGCEEPROM;
+    RW__ uint32 RCGCWTIMER;
+    vuint32 reserve[232];
+    RO__ uint32 PRWD;
+    RO__ uint32 PRTIMER;
+    RO__ uint32 PRGPIO;
+    RO__ uint32 PRDMA;
+    vuint32 reserved5;
+    RO__ uint32 PRHIB;
+    RO__ uint32 PRUART;
+    RO__ uint32 PRSSI;
+    RO__ uint32 PRI2C;
+    vuint32 reserved6;
+    RO__ uint32 PRUSB;
+    vuint32 reserved7[2];
+    RO__ uint32 PRCAN;
+    RO__ uint32 PRADC;
+    RO__ uint32 PRACMP;
+    RO__ uint32 PRPWM;
+    RO__ uint32 PRQEI;
+    vuint32 reserved8[4];
+    RO__ uint32 PREEPROM;
+    RO__ uint32 PRWTIMER;
+}SysCTRL_Type;
+
+/**********************************************************************************************************************
+ *  GLOBAL CONSTANT MACROS
+ *********************************************************************************************************************/
+//private peripherals addresses
+#define CORTEXM4_CORE_PERIPHERALS_BASE              0xE000E000
+#define NVIC_OFFSET                                 0x100
+#define SCB_OFFSET                                  0xD00
+
+//GPIO APB Base addresses
+#define GPIOA_BASE									0x40004000
+#define GPIOB_BASE									0x40005000
+#define GPIOC_BASE									0x40006000
+#define GPIOD_BASE									0x40007000
+#define GPIOE_BASE									0x40024000
+#define GPIOF_BASE									0x40025000
+
+//SysCtrl Base addresses
+#define SysCtrl_BASE                               0x400FE000
+
+//modules instances
+#define NVIC    ((NVIC_Type*)(CORTEXM4_CORE_PERIPHERALS_BASE+NVIC_OFFSET))
+#define SCB     ((SCB_Type*)(CORTEXM4_CORE_PERIPHERALS_BASE+SCB_OFFSET))
+
+#define GPIOA		((GPIO_Type*)(GPIOA_BASE))
+#define GPIOB		((GPIO_Type*)(GPIOB_BASE))
+#define GPIOC		((GPIO_Type*)(GPIOC_BASE))
+#define GPIOD		((GPIO_Type*)(GPIOD_BASE))
+#define GPIOE		((GPIO_Type*)(GPIOE_BASE))
+#define GPIOF		((GPIO_Type*)(GPIOF_BASE))
+
+#define SysCtrl     ((SysCTRL_Type*)(SysCtrl_BASE))
+
+#endif  /* MEMORYMAP_H */
+
+/**********************************************************************************************************************
+ *  END OF FILE: MemoryMap.h
+ *********************************************************************************************************************/
---
+++ tasks/Src/Common/PlatformTypes.h
@@ -0,0 +1,44 @@
+/*
+ * main.c
+ *
+ *  Created on: Jul 14, 2022
+ * @author         : ENG/Mohamed Mostafa Maher
+ * @contact        : mohamed.mostafa.maher.999@gmail.com
+ * @brief          : MY STD types
+ */
+
+#ifndef PLATFORM_TYPES_H
+#define PLATFORM_TYPES_H
+
+/*--------------------------signed integers------------------------*/
+    typedef char                            sint8;
+    typedef short                           sint16;
+    typedef int                             sint32;
+    typedef long long                       sint64;
+/*------------------------unsigned integers------------------------*/   
+    typedef unsigned char                   uint8;
+    typedef unsigned short                  uint16;
+    typedef unsigned long                   uint32;
+    typedef unsigned long long              uint64;
+/*-----------------------------doubles-----------------------------*/
+    typedef float                           float32;
+    typedef double                          float64;
+    typedef long double                     float128;
+/*-------------------volatile signed integers----------------------*/
+    typedef volatile char                   vint8;
+    typedef volatile short                  vint16;
+    typedef volatile long                   vint32;
+    typedef volatile long long              vint64;
+/*-------------------volatile unsigned integers--------------------*/
+    typedef volatile unsigned char          vuint8;
+    typedef volatile unsigned short         vuint16;
+    typedef volatile unsigned long          vuint32;
+    typedef volatile unsigned long long     vuint64;
+/*------------------------volatile doubles-------------------------*/
+    typedef volatile float                  vfloat32;
+    typedef volatile double                 vfloat64;
+    typedef volatile long double            vfloat128;
+/*-----------------------------boolean-----------------------------*/
+    typedef unsigned long                   boolean;
+    typedef volatile unsigned long          vboolean;   
+#endif
