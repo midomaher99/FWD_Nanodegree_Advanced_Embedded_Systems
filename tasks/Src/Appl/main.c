ab3adc08b25328ee361659b24a9da1302c66c03d
Mohamed Mostafa <mohamed.mostafa.maher.999@gmail.com>
2022-07-24 19:59:13 +0200

    testing Intrrupt controller -NVIC Driver-

---
+++ tasks/Src/Appl/main.c
@@ -0,0 +1,19 @@
+/*
+ * main.c
+ *
+ *  Created on: Jul 21, 2022
+ *      Author: ENG: Mohame Mostafa Maher
+ *      contact: mohamed.mostafa.maher.999@gmail.com
+ */
+
+int main()
+{
+	#include "MemoryMap.h"
+	#include "IntCtrl.h"
+
+	IntCrtl_Init();
+	while(1)
+	{
+			
+	}
+}
